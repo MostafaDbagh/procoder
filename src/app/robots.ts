ec8cb5b96216d77eda21e9477cdfd2fb2a92e5b8
Mostafa Dbagh <48655450+MostafaDbagh@users.noreply.github.com>
2026-04-13 23:43:02 +0400

    test

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ const SITE_URL = process.env.SITE_URL || "https://procoder.com";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
+      
       {
         userAgent: "*",
         allow: "/",
